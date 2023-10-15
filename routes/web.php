20e5bb1f53a46311b44a2d843a9724ad4ed7f9ed
Logan <logan.rivera5140@alumnos.udg.mx>
2023-10-15 14:00:40 -0600

    Barra de navegacion con la nueva opcion

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,13 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/articles', function () {
+        return view('articles');
+    })->name('articles');
+});
