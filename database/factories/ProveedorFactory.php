73b699be1b8ba2d085fc10db4ca332e80a2f358a
Logan <logan.rivera5140@alumnos.udg.mx>
2023-11-17 16:30:01 -0600

    Factory y seeder de proveedor terminado

---
+++ database/factories/ProveedorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Proveedor>
+ */
+class ProveedorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre_completo'=>fake()->sentence(),
+            'num_telefono'=>fake()->phoneNumber(),
+            'correo'=>fake()->email(),
+            'direccion'=>fake()->sentence(),
+            'nombre_empresa'=>fake()->sentence()
+        ];
+    }
+}
