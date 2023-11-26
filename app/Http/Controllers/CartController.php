a96e0b08a1ab99df7b31a8c725df4289460f08d7
Logan <logan.rivera5140@alumnos.udg.mx>
2023-11-26 10:38:59 -0600

    Add del carrito creo

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CartController extends Controller
+{
+    //
+}
